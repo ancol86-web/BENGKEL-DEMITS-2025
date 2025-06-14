a010d0c40ce19daaacc05c77426fad7a23322817
ancol86-web <ancolputra13@gmail.com>
2025-06-14 22:51:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Saat halaman selesai dimuat, animasi akan berjalan (bisa ditambah interaktif jika perlu)
+document.addEventListener("DOMContentLoaded", () => {
+  const logo = document.querySelector('.logo-bso');
+  logo.classList.add('fade-in-top');
+});
